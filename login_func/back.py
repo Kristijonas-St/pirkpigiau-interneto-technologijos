87bcc8bd185c8467f6b694ae9cef4f7f246147fb
miazaky <www.maksfedotenkov@gmail.com>
2025-04-01 18:37:41 +0300

    add user database

--- login_func/back.py
+++ login_func/back.py
@@ -1,34 +1,43 @@
 from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
 
-back = Flask(__name__)
+app = Flask(__name__)
 
-@back.route('/login', methods=['POST'])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
 
-    res = make_response(jsonify(success=True))
-    
-    if username == 'adas' and password == 'padas':
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.password == password:
+        res = make_response(jsonify(success=True))
         res.set_cookie('session', 'logged_in')
-    elif username == 'a' and password == 'a':
-        res.set_cookie('session', 'guest')
+        return res
     else:
-        return jsonify(success=False), 401
+        return jsonify(success=False, message="Invalid credentials"), 401
 
-    print(f"User {username} logged in with session: {res.headers.get('Set-Cookie')}")
-    return res
-
-@back.route('/protected')
+@app.route('/protected')
 def protected():
-    session_cookie = request.cookies.get('session')  
-    print(f"Session Cookie Received: {session_cookie}")
-
-    if session_cookie == 'logged_in': 
+    session_cookie = request.cookies.get('session')
+    if session_cookie == 'logged_in':
         return jsonify(access=True)
     else:
         return jsonify(access=False), 403
 
 if __name__ == "__main__":
-    back.run(debug=True)
+    app.run(debug=True)
