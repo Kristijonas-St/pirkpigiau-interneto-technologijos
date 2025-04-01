4cb35a7400ce845e2bfa22799a687f40c068f503
miazaky <www.maksfedotenkov@gmail.com>
2025-04-01 18:52:59 +0300

    add hashed passwords feature to user database

--- login_func/back.py
+++ login_func/back.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -7,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,7 +26,7 @@ def login():
 
     user = User.query.filter_by(username=username).first()
 
-    if user and user.password == password:
+    if user and bcrypt.check_password_hash(user.password, password):
         res = make_response(jsonify(success=True))
         res.set_cookie('session', 'logged_in')
         return res
@@ -38,6 +40,35 @@ def protected():
         return jsonify(access=True)
     else:
         return jsonify(access=False), 403
+
+#Tam, kad butu hashed passwordas database ir veiktu loginas reikia addint per postmana
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(success=True, message="User added successfully!")
+
+#Jeigu reikes istrinti useri
+@app.route('/delete_user', methods=['DELETE'])
+def delete_user():
+    data = request.get_json()
+    username = data.get('username')
+
+    deleted_count = db.session.query(User).filter_by(username=username).delete()
+
+    if deleted_count > 0:
+        db.session.commit()
+        return jsonify(success=True, message="User deleted successfully!")
+    else:
+        return jsonify(success=False, message="User not found!"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
